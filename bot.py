9b3dfafd6347719d0141f12538a91d8fd4e8d03c
Unanoc <leedaniil98@gmail.com>
2018-09-12 01:57:44 +0300

    almost works

--- bot.py
+++ bot.py
@@ -4,21 +4,26 @@ from datetime import datetime
 from logger.logger import logger
 import telebot
 import bmstu_schedule
+import re
 
 bot = telebot.TeleBot(access_token)
+reg = re.compile("^[а-яА-Я]{0,4}\d{0,2}\-\d{0,2}[а-яА-Я]?$")
 
 @bot.message_handler(commands=["start"])
 def start(message):
     logger(message)
     bot.send_message(message.chat.id, text="Привет, {} {}!".format(message.chat.first_name, message.chat.last_name))
 
-
-
-
 @bot.message_handler(content_types=["text"])
-def anyMessages(message):
+def any_messages(message):
     logger(message)
-    bot.send_message(message.chat.id, text="Чёт я ничего не нашел для группы {}. Если проблема и правда во мне, то напиши @lee_daniil".format(message.text))
+    message_text = message.text
+    if (reg.match(message_text) != None):
+        dt = datetime.strptime('2018-09-03', '%Y-%m-%d')
+        bmstu_schedule.run(message_text, dt, "/Users/lee/Documents/GitHub/telegram-bmstu-schedule-bot/vault")
+        bot.send_message(message.chat.id, text="Получилось!")
+    else:
+        bot.send_message(message.chat.id, text="Чёт я ничего не нашел для группы '{}'. Если проблема и правда во мне, то напиши @lee_daniil".format(message_text))
 
 
 if __name__ == '__main__':
@@ -26,6 +31,3 @@ if __name__ == '__main__':
         bot.polling(none_stop=True, timeout=30)
     except Exception as ex:
         pass
-
-# dt = datetime.strptime('2018-09-03', '%Y-%m-%d')
-# bmstu_schedule.run("ИУ6-54", dt, "/Users/lee/Documents/GitHub/telegram-bmstu-schedule-bot/vault")
