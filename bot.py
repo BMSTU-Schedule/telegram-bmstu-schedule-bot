42e8390968f06d4c214865387a77546cbdf67d59
Unanoc <leedaniil98@gmail.com>
2018-09-13 00:27:38 +0300

    it alomost works but i should fix it

--- bot.py
+++ bot.py
@@ -8,14 +8,15 @@ import re
 import telebot
 
 bot = telebot.TeleBot(access_token)
-reg = re.compile("^[а-яА-Я]{1,4}\d{0,2}\-\d{0,2}[а-яА-Я]?$")
+reg = re.compile('^[а-яА-Я]{1,4}\d{0,2}\-\d{0,2}[а-яА-Я]?$')
 path_to_vault = '/Users/lee/Documents/GitHub/telegram-bmstu-schedule-bot/vault/'
 dt = datetime.strptime('2018-09-03', '%Y-%m-%d')
+schedule_file = '{}Расписание {}.ics'
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=['start'])
 def start(message):
     logger(message)
-    bot.send_message(message.chat.id, text="Привет, {} {}!".format(message.chat.first_name, message.chat.last_name))
+    bot.send_message(message.chat.id, text='Привет, {} {}!'.format(message.chat.first_name, message.chat.last_name))
 
 
 
@@ -26,26 +27,35 @@ def group_validator(group_id):
         return False
 
 def file_exist(file_name):
-    return os.path.isfile("{}Расписание {}Б.ics".format(path_to_vault,file_name))
+    return os.path.isfile(schedule_file.format(path_to_vault,file_name))
 
 
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(content_types=['text'])
 def any_messages(message):
     logger(message)
 
     if group_validator(message.text):
+        bot.send_message(message.chat.id, text='Пошел искать расписание для группы {}'.format(message.text))
+
         if file_exist(message.text):
-            file_to_send = open("{}Расписание {}Б.ics".format(path_to_vault,message.text), 'rb')
-            bot.send_document(message.chat.id, file_to_send)
+            file_to_send = open(schedule_file.format(path_to_vault, message.text), 'rb')
         else:
             bmstu_schedule.run(message.text, dt, path_to_vault)
             if file_exist(message.text):
-                file_to_send = open("{}Расписание {}.ics".format(path_to_vault,message.text), 'rb')
+                file_to_send = open(schedule_file.format(path_to_vault, message.text), 'rb')
             else:
-                bot.send_message(message.chat.id, text="Не получилось!!!")
+                if message.text[len(message.text)-1].isnumeric():
+                    bot.send_message(message.chat.id, text='Чёт я ничего не нашел для группы {}. Если проблема и правда во мне, то напиши @gabolaev'.format(message.text))
+                    return
+                bot.send_message(message.chat.id, text="Эээ, кажется, кто-то не уточнил тип своей группы (Б/М/А). Давай добавим соответствующую букву в конце и попробуем еще раз. Например {}Б".format(message.text))
+                return
+        
+        bot.send_document(message.chat.id, file_to_send)
+        bot.send_message(message.chat.id, text='Тадам!')
+        bot.send_message(message.chat.id, text='Если вдруг будут проблемы при импорте в календарь, можешь обращаться к @lee_daniil')
     else:
-        bot.send_message(message.chat.id, text="Чёт я ничего не нашел для группы '{}'. Если проблема и правда во мне, то напиши @lee_daniil".format(message.text))
+        bot.send_message(message.chat.id, text='Указан неверный формат номера группы.')
 
     
 if __name__ == '__main__':
