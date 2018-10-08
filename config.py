8a63aa247aa6442cc5ee842c7adb37c445bfb66c
George <gabolaev98@gmail.com>
2018-10-08 21:27:38 +0300

    added regex prefix

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ Configuration file
 TOKEN = 'TELEGRAM BOT TOKEN'
 # vault - folder with downloaded .ics
 VAULT_PATH = 'ABSOLUTE PATH TO VAULT FOLDER'
-GROUP_CODE_REGEX = '^[а-яА-Я]{1,4}\d{0,2}\-\d{0,3}[а-яА-Я]?$'
+GROUP_CODE_REGEX = r'^[а-яА-Я]{1,4}\d{0,2}\-\d{0,3}[а-яА-Я]?$'
