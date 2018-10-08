100fe219508c20601ffa040ca58a34ec03c5d31a
George <gabolaev98@gmail.com>
2018-10-08 21:17:33 +0300

    Some format changes according to pylint warnings

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-filesaccess_token = 'TELEGRAM BOT TOKEN'
-path_to_vault = 'ABSOLUTE PATH TO VAULT FOLDER' # vault - folder with downloaded .ics
+'''
+Configuration file
+'''
+
+TOKEN = 'TELEGRAM BOT TOKEN'
+# vault - folder with downloaded .ics
+VAULT_PATH = 'ABSOLUTE PATH TO VAULT FOLDER'
+GROUP_CODE_REGEX = '^[а-яА-Я]{1,4}\d{0,2}\-\d{0,3}[а-яА-Я]?$'
