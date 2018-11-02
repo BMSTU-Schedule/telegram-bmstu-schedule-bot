266a5ef908a3b4e60f6ffb182eb7971255ae7215
George <gabolaev98@gmail.com>
2018-11-02 22:55:42 +0000

    merge conflict fixed

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 Configuration file
 '''
 
-TOKEN = 'TELEGRAM BOT TOKEN'
-# vault - folder with downloaded .ics
-VAULT_PATH = 'ABSOLUTE PATH TO VAULT FOLDER'
 GROUP_CODE_REGEX = r'^[а-яА-Я]{1,4}\d{0,2}\-\d{0,3}[а-яА-Я]?$'
+TOKEN = 'TOKEN'
+VAULT_PATH = '/var/bs_mount'
