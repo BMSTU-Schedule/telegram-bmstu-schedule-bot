cd90baf67fdbb1065ad99dd555197c9eeabe48a7
Daniil Lee <leedaniil98@gmail.com>
2018-11-03 02:07:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 Configuration file
 '''
 
-GROUP_CODE_REGEX = r'^[а-яА-Я]{1,4}\d{0,2}\-\d{0,3}[а-яА-Я]?$'
+GROUP_CODE_REGEX = r'^[а-яА-Я]{1,4}\d{0,2}\-\d{1,3}[а-яА-Я]?$'
 TOKEN = 'TOKEN'
 VAULT_PATH = '/var/bs_mount'
