70572f23b0f98ac6fc7643887e9609441bbbe710
Unanoc <leedaniil98@gmail.com>
2019-01-28 15:16:36 +0300

    refactoring of config.py, added config.json

--- config.py
+++ config.py
@@ -2,6 +2,16 @@
 Configuration file
 '''
 
+import re
+import json
+from datetime import datetime
+
 GROUP_CODE_REGEX = r'^[а-яА-Я]{1,4}\d{0,2}\-\d{1,3}[а-яА-Я]?$'
-TOKEN = 'TOKEN'
-VAULT_PATH = '/var/bs_vault'
+GC_REG_MATCHER = re.compile(GROUP_CODE_REGEX)
+DT = datetime.strptime('2018-09-03', '%Y-%m-%d')
+SCHEDULE_ICS = '{}/{}.ics'
+SCHEDULE_PNG = '{}/{}.png'
+
+CONFIG = {}
+with open('config.json', 'r') as f:
+    CONFIG = json.loads(f.read())
