98655b610d6ad37489969990382bcfb5d9549e17
George Gabolaev <g.gabolaev@corp.mail.ru>
2019-08-31 17:45:57 +0300

    env config

--- bmstu_schedule_bot/configs/config.py
+++ bmstu_schedule_bot/configs/config.py
@@ -7,14 +7,19 @@ import os
 import requests
 from datetime import datetime
 
-CONFIG = {}
-with open('config.json', 'r') as f:
-    CONFIG = json.loads(f.read())
+CONFIG = {
+    'token': os.environ.get('ACCESS_TOKEN'),
+    'vault': os.environ.get('VAULT'),
+    'dialogue': os.environ.get('DIALOGUE_CFG_PATH'),
+    'start_date_api_url': os.environ.get('START_DATE_API_URL'),
+    'logfile': os.environ.get('LOG_FILE'),
+    'doctors': os.environ.get('DOCTORS'),
+}
 
 if not os.path.exists(CONFIG['vault']):
     os.makedirs(CONFIG['vault'])
 
-r = requests.get(url=CONFIG['url_api_start_date'])
+r = requests.get(url=CONFIG['start_date_api_url'])
 start_date_json = r.json()
 SEM_START_DATE = datetime.strptime(
     start_date_json['semester_start_date'],
